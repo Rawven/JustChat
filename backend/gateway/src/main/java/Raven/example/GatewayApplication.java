e933665ff23aa442f73d587f46685e384c422220
liu <a2506991786@qq.com>
2023-11-22 00:56:28 +0800

    init gateway

---
+++ backend/gateway/src/main/java/Raven/example/GatewayApplication.java
@@ -0,0 +1,17 @@
+package Raven.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * gateway application
+ *
+ * @author 刘家辉
+ * @date 2023/11/22
+ */
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class, args);
+    }
+}
