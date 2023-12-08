9492bd5945d5dcb75fa85c65e7c25e1deca11c2d
liu <a2506991786@qq.com>
2023-12-08 18:45:48 +0800

    引入tailwind css

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+//tailwind.config.js
+// eslint-disable-next-line no-undef
+module.exports = {
+  content: [
+    "./src/components/test/*.vue",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
