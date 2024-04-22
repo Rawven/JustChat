c3af1b18fd08d4a1f5e7e76fa02c9aaef1126ff0
liu <a2506991786@qq.com>
2024-04-22 16:54:49 +0800

    feat(): 接入xxl-job解决分布式定时任务问题

--- backend/jc-im/src/main/java/www/raven/jc/schedule/ImSchedule.java
+++ backend/jc-im/src/main/java/www/raven/jc/schedule/ImSchedule.java
@@ -1,11 +1,11 @@
 package www.raven.jc.schedule;
 
+import com.xxl.job.core.handler.annotation.XxlJob;
 import jakarta.websocket.Session;
 import lombok.extern.slf4j.Slf4j;
 import org.redisson.api.RScoredSortedSet;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import www.raven.jc.constant.OfflineMessagesConstant;
 import www.raven.jc.ws.WebsocketService;
@@ -19,7 +19,6 @@ import www.raven.jc.ws.WebsocketService;
 @Slf4j
 @Component
 public class ImSchedule {
-
     /**
      * 默认WebSocket连接过期时间为1小时
      */
@@ -29,25 +28,38 @@ public class ImSchedule {
      * 默认离线消息过期时间为7天
      */
     private static final long OFFLINE_MESSAGE_EXPIRATION_TIME = 7 * 24 * 60 * 60 * 1000;
-
     @Autowired
     private RedissonClient redissonClient;
 
     /**
      * 定期清理不活跃的WebSocket连接
      */
-    @Scheduled(fixedRate = 60 * 1000)
+    @XxlJob(value = "deleteOfflineWsHandler")
     public void checkRoomWs() {
+        log.info("--xxl-job--清理不活跃的WebSocket连接");
         long currentTime = System.currentTimeMillis();
-        for (WebsocketService websocketService : WebsocketService.webSockets) {
-            if ((currentTime - websocketService.getLastActivityTime()) > WS_EXPIRATION_TIME) {
-                closeRoomExpiredConnection(websocketService);
+        for (WebsocketService websocket : WebsocketService.webSockets) {
+            if ((currentTime - websocket.getLastActivityTime()) > WS_EXPIRATION_TIME) {
+                Session session = websocket.getSession();
+                if (session != null && session.isOpen()) {
+                    try {
+                        session.close();
+                    } catch (Exception e) {
+                        log.error("关闭过期连接失败");
+                    }
+                }
+                WebsocketService.webSockets.remove(websocket);
+                log.info("WebSocket连接过期，用户id为{},总数为:{}", websocket.getUserId(), WebsocketService.webSockets.size());
             }
         }
     }
 
-    @Scheduled(fixedRate = 60 * 1000 * 60)
+    /**
+     * 定期清理过期的离线消息
+     */
+    @XxlJob(value = "deleteOfflineMessageHandler")
     public void checkOfflineMessage() {
+        log.info("--xxl-job--清理过期的离线消息");
         long currentTime = System.currentTimeMillis();
         //遍历删除所有用户的过期离线消息
         for (String key : redissonClient.getKeys().getKeysByPattern(OfflineMessagesConstant.PREFIX_MATCH)) {
@@ -58,16 +70,4 @@ public class ImSchedule {
         }
     }
 
-    private void closeRoomExpiredConnection(WebsocketService websocket) {
-        Session session = websocket.getSession();
-        if (session != null && session.isOpen()) {
-            try {
-                session.close();
-            } catch (Exception e) {
-                log.error("关闭过期连接失败");
-            }
-        }
-        WebsocketService.webSockets.remove(websocket);
-        log.info("WebSocket连接过期，用户id为{},总数为:{}", websocket.getUserId(), WebsocketService.webSockets.size());
-    }
 }
